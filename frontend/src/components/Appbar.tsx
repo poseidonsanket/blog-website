4611c3c1ba38b0fc7a9d2f58f2efa675697cb265
Sanket Dadali <sanketdadali05@gmail.com>
2024-06-19 19:03:20 +0530

    Publish Blog Feature Added and responsive changes

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -2,7 +2,7 @@ import { Avatar } from "./BlogCard"
 import { Link } from "react-router-dom"
 
 export const Appbar = () => {
-    return <div className="border-b flex justify-between px-10 py-4">
+    return <div className="border-b flex justify-between px-5 lg:px-10 py-4">
         <Link to={'/blogs'} className="flex flex-col justify-center cursor-pointer font-bold text-2xl">
                 Medium
         </Link>
@@ -12,6 +12,8 @@ export const Appbar = () => {
             </Link>
 
             <Avatar size={"big"} name="Sanket" />
+
+            <button type="button" className="ml-4 text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">Sign Out</button>
         </div>
     </div>
 }

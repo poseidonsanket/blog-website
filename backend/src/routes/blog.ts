b21a681ee318d71b7a333e879d8fbce5a1939a28
Sanket Dadali <sanketdadali05@gmail.com>
2024-06-19 18:40:58 +0530

    Backend Changes and Blogs fetching Done

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -98,7 +98,18 @@ blogRouter.get("/bulk", async (c) => {
     datasourceUrl: c.env?.DATABASE_URL,
   }).$extends(withAccelerate());
 
-  const posts = await prisma.post.findMany({});
+  const posts = await prisma.post.findMany({
+    select: {
+      content: true,
+      title: true,
+      id: true,
+      author: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
 
   return c.json(posts);
 });
@@ -112,6 +123,15 @@ blogRouter.get("/:id", async (c) => {
   const post = await prisma.post.findUnique({
     where: {
       id,
+    },
+    select: {
+      content: true,
+      title: true,
+      author: {
+        select: {
+          name: true,
+        },
+      },
     },
   });
 
--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -15,7 +15,7 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
     async function sendRequest() {
         try {
             const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`, postInputs);
-            const jwt = response.data.jwt;
+            const jwt = "Bearer " + response.data.jwt;
             localStorage.setItem("token", jwt);
             navigate("/blogs");
         } catch(e) {
